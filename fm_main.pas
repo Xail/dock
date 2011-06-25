bb563dc2b99b72518d8e1325a44b486674d18baa
Михаил Гладких <mgladk@gmail.com>
2011-06-26 00:38:39 +0600

    Добавлена панель для вызова меню Пуск Правда меню вызыватеся там, где находится кнопка пуск

--- fm_main.pas
+++ fm_main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-  ExtCtrls, Buttons, DockPanel;
+  ExtCtrls, Buttons, DockPanel, Windows;
 
 type
 
@@ -14,10 +14,12 @@ type
 
   TDockForm = class(TForm)
     DockPanel: TDockPanel;
+    Panel1: TPanel;
     Timer1: TTimer;
     procedure DockPanelResize(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormResize(Sender: TObject);
+    procedure Panel1Click(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
   private
 
@@ -50,6 +52,12 @@ procedure TDockForm.FormResize(Sender: TObject);
 begin
   Top := Screen.Height - Height;
   Left:= (Screen.Width - Width) div 2;
+end;
+
+procedure TDockForm.Panel1Click(Sender: TObject);
+begin
+  SendMessage(Handle, WM_SYSCOMMAND, SC_TASKLIST, word(Mouse.CursorPos.x) +
+  	word(Mouse.CursorPos.y << 16));
 end;
 
 procedure TDockForm.Timer1Timer(Sender: TObject);
